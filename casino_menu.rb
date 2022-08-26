7c32e5d3070cc30aa302f7fa3d557a9d0385f4a0
Christian Green <hoolio.suare@gmail.com>
2022-08-25 20:37:27 -0600

    removed game 4

--- casino_menu.rb
+++ casino_menu.rb
@@ -1,23 +1,45 @@
-def menu
-  puts "Please Make a Selection"
-  puts "1) Slots"
-  puts "2) Roulette"
-  puts "3) HighLow"
-  puts "4) Exit"
-  menu_choice = gets.strip.to_i
+require_relative 'player'
+require_relative 'slots'
 
-  if menu_choice == 1
-    Slots
-  elsif menu_choice == 2
-    Roulette
-  elsif menu_choice == 3
-     HighLow
-  elsif menu_choice ==4
-    puts "Thank you for playing!"
-    exit
-  else 
-    puts "Sorry, you need to enter a number 1-4"
-  end
-  menu
+class Mainmenu
+    attr_accessor :player
+
+    def initialize
+        @player = create_player
+    end
+
+    def create_player
+        puts "Enter your name: "
+        name = gets.strip
+        puts "Enter how much money you brought: "
+        money = gets.strip.to_i
+
+        @player = Player.new(name, money)
+        menu
+    end
+
+    def menu
+        puts "hello #{player.name}"
+        puts "Please Make a Selection"
+        puts "1) Slots"
+        puts "2) Roulette"
+        puts "3) Game 3"
+        puts "4) Exit"
+        menu_choice = gets.strip.to_i
+        
+        if menu_choice == 1
+            @slots = Slotgame.new(@player)
+        elsif menu_choice == 2
+            Roulette
+        elsif menu_choice == 3
+            Game 3
+        elsif menu_choice == 4
+            puts "Thank you for playing!"
+            exit
+        else 
+            puts "Sorry, you need to enter a number 1-5"
+        end
+    end
 end
-menu
+
+menu = Mainmenu.new()
