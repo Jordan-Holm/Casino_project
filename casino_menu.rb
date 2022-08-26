37c51b715dc1b7f64d73f93a10a33e1a71abc145
Christian Green <hoolio.suare@gmail.com>
2022-08-25 19:27:33 -0600

    removed menu option

--- casino_menu.rb
+++ casino_menu.rb
@@ -1,9 +1,22 @@
-
 def menu
   puts "Please Make a Selection"
   puts "1) Slots"
   puts "2) Roulette"
-  puts "3) Game 3"
-  puts "4) Game 4"
-  puts "5) Exit"
+  puts "3) HighLow"
+  puts "4) Exit"
+  menu_choice = gets.strip.to_i
+
+  if menu_choice == 1
+    Slots
+  elsif menu_choice == 2
+    Roulette
+  elsif menu_choice == 3
+     HighLow
+  elsif menu_choice ==4
+    puts "Thank you for playing!"
+    exit
+  else 
+    puts "Sorry, you need to enter a number 1-4"
+  end
+  menu
 end
