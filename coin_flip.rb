1e7dbf6dfc85ec1d7215d9cca76a1c5bf6a94e5c
Christian Green <hoolio.suare@gmail.com>
2022-08-30 21:37:53 -0600

    fixed coinflip win and lose issue

--- coin_flip.rb
+++ coin_flip.rb
@@ -16,53 +16,60 @@ class Coingame
 
   def run_game(bet_amount)
     winnings = bet_amount * 6
+    coin_flip_num = rand(1..2)
 
-  puts "Choose: 1) Heads  2) Tails"
-  user_input = gets.to_i
-  
-  #issue area!! below
-  if rand.round == 0
-    coin = "Heads!"
-  else
-    coin = "tails"
-  end
-
-  if coin.to_i == user_input.to_i
-    player.bankroll_adjust(winnings)
-    puts "And the coin lands on " + coin.upcase + "! YOU WIN!!!"
-    puts "Total: #{player.money}"
-  else
-    player.bankroll_adjust(-bet_amount)
-    puts "And the coin lands on " + coin + "...ouch, I'm not sure if you'll ever financially recover from this."
-    puts "      ______________________________
-   / \                                   \.
-   |   |                            |.
-    \_ |                            |.
-       |                            |.
-       |                            |.
-       |                            |.
-       |      you lost #{bet_amount}!          |.
-       |                            |.
-       |                            |.
-       |                            |.
-       |                            |.
-       |                            |.
-       |                            |.
-       |                            |.
-       |   _________________________|___
-       |  /                            /.
-       \_/____________________________/."
-    puts "Total: #{player.money}"
-  end
-
-# This method will iterate over each line individually from the .txt file and print it to the console
-  def render_ascii_art
-  File.readlines("art.txt") do |line|
-    puts line
-      end
+    puts "Choose: 1) Heads  2) Tails"
+    user_input = gets.to_i
+    if user_input == 1
+      user_coinside = "Heads!"
+    else 
+      user_coinside = "tails"
     end
 
-    # add ascII art
-# maybe ascii art for slots icons?
-  end
+    
+    #issue area!! below
+    if coin_flip_num == 1
+      coin = "Heads!"
+    else
+      coin = "tails"
+    end
+
+    if coin == user_coinside
+      player.bankroll_adjust(winnings)
+      puts "And the coin lands on " + coin.upcase + "! YOU WIN!!!"
+      puts "Total: #{player.money}"
+    else
+      player.bankroll_adjust(-bet_amount)
+      puts "And the coin lands on " + coin + "...ouch, I'm not sure if you'll ever financially recover from this."
+      puts "      ______________________________
+    / \                                   \.
+    |   |                            |.
+      \_ |                            |.
+        |                            |.
+        |                            |.
+        |                            |.
+        |      you lost #{bet_amount}!          |.
+        |                            |.
+        |                            |.
+        |                            |.
+        |                            |.
+        |                            |.
+        |                            |.
+        |                            |.
+        |   _________________________|___
+        |  /                            /.
+        \_/____________________________/."
+      puts "Total: #{player.money}"
+    end
+
+  # This method will iterate over each line individually from the .txt file and print it to the console
+    def render_ascii_art
+    File.readlines("art.txt") do |line|
+      puts line
+        end
+      end
+
+      # add ascII art
+  # maybe ascii art for slots icons?
+    end
 end
